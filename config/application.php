b71fdf8c515a87cd0002750c1651b7a9a2b0be93
Laurie <lauriehuw@gmail.com>
2016-11-22 15:13:19 +0100

    remove

--- config/application.php
+++ config/application.php
@@ -84,10 +84,3 @@ if (!defined('ABSPATH')) {
 
 /* Multisite */
 define( 'WP_ALLOW_MULTISITE', true );
-
-define('MULTISITE', true);
-define('SUBDOMAIN_INSTALL', false);
-define('DOMAIN_CURRENT_SITE', 'bedrock-multinetwork.herokuapp.com');
-define('PATH_CURRENT_SITE', '/');
-define('SITE_ID_CURRENT_SITE', 1);
-define('BLOG_ID_CURRENT_SITE', 1);
